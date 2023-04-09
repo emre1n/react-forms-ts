1b6dce92cb32bcded59c3aea249740d0796d9ff0
emre1n <emre1n@protonmail.com>
2023-04-09 14:07:33 +0300

    add enteredName state

--- src/components/SimpleInput/index.tsx
+++ src/components/SimpleInput/index.tsx
@@ -1,9 +1,25 @@
+import { useRef, useState } from 'react';
+
 const SimpleInput = () => {
+  const [enteredName, setEnteredName] = useState('');
+
+  const nameInputChangeHandler = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    setEnteredName(event.target.value);
+  };
+
+  const formSubmissonHandler = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    console.log('enteredName', enteredName);
+  };
+
   return (
-    <form>
+    <form onSubmit={formSubmissonHandler}>
       <div className="form-control">
         <label htmlFor="name">Your Name</label>
-        <input type="text" id="name" />
+        <input type="text" id="name" onChange={nameInputChangeHandler} />
       </div>
       <div className="form-actions">
         <button>Submit</button>
